cb48b1ab2171b5cb14cc539197877546841f874f
James Hines <jameshines10@gmail.com>
2017-03-21 12:24:22 -0400

    Adds postal in order to create a message bus. Started over in main.js to experiment with postal

--- js/main.js
+++ js/main.js
@@ -1,38 +1,26 @@
-import EventBus from './eventbus'
 import UserProfileComponent from './components/UserProfileComponent'
 import ButtonComponent from './components/ButtonComponent'
 
-/*
-A component should publish and subscribe to events
-Pass the event bus object to the component constructor
-*/
+import postal from 'postal'
 
-let eventBus = new EventBus();
+var subscription = postal.subscribe({
+    channel: "orders",
+    topic: "item.add",
+    callback: function(data, envelope) {
+        // `data` is the data published by the publisher. 
+        // `envelope` is a wrapper around the data & contains 
+        // metadata about the message like the channel, topic, 
+        // timestamp and any other data which might have been 
+        // added by the sender.
+        console.log(data);
+    }
+});
 
-let container = document.getElementById('casey');
-let caseyComponent = new UserProfileComponent(eventBus, container)
-
-container = document.getElementById('james');
-let jamesComponent = new UserProfileComponent(eventBus, container);
-
-let button = new ButtonComponent(eventBus);
-
-let profileEvent = {
-  type: 'UPDATE_PROFILE',
-  componentName: 'userProfile',
-  eventId: 1,
-  userId: 1,
-  payload: {  	
-    userName: 'semaj',
-    firstName: 'James',
-    lastName: 'Hines',
-    email: 'jameshines10@gmail.com'  
-  }
-}
-
-caseyComponent.render('Casey');
-jamesComponent.render('James');
-
-setTimeout(function(){ 
-	caseyComponent.render('Casey McCarty');
-}, 3000);
+postal.publish({
+    channel: "orders",
+    topic: "item.add",
+    data: {
+        sku: "AZDTF4346",
+        qty: 21
+    }
+});
