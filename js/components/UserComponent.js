caaf9f00b2f52cb9e522232c7e3cafd761a9f89a
James Hines <jameshines10@gmail.com>
2017-03-21 22:28:49 -0400

    The UserComponent can now subscribe to multiple topics

---
+++ js/components/UserComponent.js
@@ -0,0 +1,78 @@
+let snabbdom = require('snabbdom');
+
+let patch = snabbdom.init([ // Init patch function with chosen modules
+  require('snabbdom/modules/class').default, // makes it easy to toggle classes
+  require('snabbdom/modules/props').default, // for setting properties on DOM elements
+  require('snabbdom/modules/style').default, // handles styling on elements with support for animations
+]);
+
+let h = require('snabbdom/h').default; // helper function for creating vnodes
+
+import postal from 'postal'
+
+function isEventForComponent(subscriptions) {
+	return (event) => {
+		//return event.topic === topic && event.componentName === componentName;
+		return subscriptions.hasOwnProperty(event.topic);
+	}			
+}
+
+function replay(events) {
+	//let reversed = events.reverse();
+
+	return events.reduce(function(state, event) {		
+		state = event.data
+		return state;
+	}, ''); 	
+}
+
+// takes in the reduced component state and returns a vnode
+function view(state, name) {
+
+	return h('div', [
+		h('div', `This is the ${name} component`),
+		h('div', state),
+		h('hr')
+	]);
+}
+
+function updateDOM(container, newVnode) {
+	return patch(container, newVnode);
+}
+
+export default class UserComponent {
+	constructor(container, eventStore, name) {
+		this._eventStore = eventStore;		
+		this._container = container;
+		this._componentName = name;
+		this._subscriptions = {};
+	}
+
+	subscribe(channel, topic) {
+		let subscription = postal.subscribe({
+		    channel: channel,
+		    topic: topic,
+		    callback: function(data, envelope) {
+		    	this.render();
+		    }.bind(this)
+		});
+
+		this._subscriptions[topic] = subscription
+
+		return subscription;
+	}
+
+	getSubscriptions() {
+		return this._subscriptions;
+	}
+
+	render() {
+		let events = this._eventStore.filter(isEventForComponent(this._subscriptions));
+
+		let reducedState = replay(events);
+		console.log(`Reduced state for ${this._componentName}: ${reducedState}`);
+
+        const newVnode = view(reducedState, this._componentName);
+		this._container = updateDOM(this._container, newVnode);
+	}
+}
