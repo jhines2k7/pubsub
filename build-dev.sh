6d5f83c5cd965e1abed959b6ac98a4c8ed649394
James Hines <jameshines10@gmail.com>
2017-03-23 11:16:36 -0400

    Creates a script for building for development purposes

---
+++ build-dev.sh
@@ -0,0 +1,7 @@
+# clean the build directory
+rm build/*
+
+# js transform
+node_modules/.bin/webpack
+
+date; echo;
--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+# clean the build directory
+rm build/*
+
 # js transform
 node_modules/.bin/webpack
 
