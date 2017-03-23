863a62e95b127ad612fad1efe1dc85ccca2870b2
James Hines <jameshines10@gmail.com>
2017-03-22 22:50:56 -0400

    Begining a refactor in order to render a component in a testable way

--- js/components/JamesComponent.js
+++ js/components/JamesComponent.js
@@ -18,44 +18,31 @@ function isEventForComponent(subscriptions) {
 	}			
 }
 
-function replay(events) {
-	//let reversed = events.reverse();
-
-	return events.reduce(function(state, event) {
-		if (event.eventType === 'async.success') {
-			state.asyncData = event.data;
-		} else if(event.eventType === 'async.error'){
-			state.asyncError = event.data;
-		} else if(event.eventType === 'click') {
-			state.syncData = event.data;
-		}
-
-		return state;
-	}, {}); 	
-}
-
 // takes in the reduced component state and returns a vnode
 function view(state, component) {
-	let dynamic = [];
+	let dynamic;
+	let urlList;
 
 	let nested = new NestedComponent(document.createElement('div'), component._eventStore);
 
 	if(state.asyncData) {
-		dynamic = state.asyncData.chapterUrls.map(function(url){
+		urlList = state.asyncData.chapterUrls.map(function(url){
 			return h('li', url);
 		});
+
+		dynamic = h('ul', urlList);
 	}
 
 	if(state.asyncError) {
-		dynamic = h('li', `${state.asyncError.message}`);
+		dynamic = h('h3', `${state.asyncError.message}`);
 	}
 	
 	return h('div', [
 		h('div', typeof state.syncData === 'undefined' ? '' : state.syncData),
 		h('div', `Async data ${typeof state.asyncData === 'undefined' ? '' : state.asyncData.heading}`),
 		h('h1', `${typeof state.asyncData === 'undefined' ? '' : state.asyncData.heading}`),
-		nested.render(),
-		h('ul', dynamic),
+		state.nestedData ? nested.render() : null,
+		dynamic,
 		h('hr')
 	]);
 }
@@ -76,7 +63,11 @@ export default class UserComponent {
 		    channel: channel,
 		    topic: topic,
 		    callback: function(data, envelope) {
-		    	this.render();
+		    	let events = this._eventStore.filter(isEventForComponent(this._subscriptions));
+
+				let reducedState = this.replay(events);
+
+		    	this.render(reducedState);
 		    }.bind(this)
 		});
 
@@ -104,14 +95,27 @@ export default class UserComponent {
 		return this._subscriptions;
 	}
 
-	render() {
-		let events = this._eventStore.filter(isEventForComponent(this._subscriptions));
-
-		let reducedState = replay(events, this);
-
+	render(reducedState) {		
         const newVnode = view(reducedState, this);
 		this._container = updateDOM(this._container, newVnode);
 
 		return this._container;
+	}
+
+	function replay(events) {
+		return events.reduce(function(state, event) {
+			if (event.eventType === 'async.success') {
+				state.asyncData = event.data;
+				state.nestedData = true;
+			} else if(event.eventType === 'async.error'){
+				state.asyncError = event.data;
+				state.nestedData = false;
+			} else if(event.eventType === 'click') {
+				state.syncData = event.data;
+				state.nestedData = true;
+			}
+
+			return state;
+		}, {}); 	
 	}
 }
