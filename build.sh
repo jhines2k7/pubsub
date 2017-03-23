2d463dc23d7b345dbbdbd58753645cafca5e4af2
James Hines <jameshines10@gmail.com>
2017-03-23 11:05:57 -0400

    Modifies the build script to update the src property of the script tag in index.html. Updates the url of the story.json file

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
 # js transform
 node_modules/.bin/webpack
 
+# copy index.html to build directory
+cp index.html build
+
+# copy story.json to build directory
+cp story.json build
+
+# change the src property of the script tag to app.js
+sed -i 's/build\/app.js/app.js/g' build/index.html
+
+# change the url to 'story.json'
+sed -i "s/url: '..\/story.json'/url: 'story.json'/g" build/app.js
+
 date; echo;
