df3f35e633da3f67aa7d46eba7ce9e4e14b1b2c6
James Hines <jameshines10@gmail.com>
2017-03-23 12:44:58 -0400

    Updates the Dockerfile

--- scripts/build.sh
+++ scripts/build.sh
@@ -17,9 +17,11 @@ cp index.html build
 cp story.json build
 
 # change the src property of the script tag to app.js
-sed -i 's/build\/app.js/app.js/g' build/index.html
+sed -i 's/build-dev\/app.js/app.js/g' build/index.html
 
 # change the url to 'story.json'
 sed -i "s/url: '..\/story.json'/url: 'story.json'/g" build/app.js
+
+docker build -t jhines2017/pubsub .
 
 date; echo;
